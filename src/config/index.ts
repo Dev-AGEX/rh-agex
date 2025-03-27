1a2e402110df2352b6c3ca7b42da45f8088e607c
CristofferFernandoNicoletti <88545881+CristofferFernandoNicoletti@users.noreply.github.com>
2025-03-27 09:54:13 -0300

    Update API

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
   CMS_URL: process.env.NEXT_PUBLIC_CMS_URL || "https://directus.agex.com.br",
-  CMS_TOKEN: process.env.NEXT_PUBLIC_CMS_TOKEN || "jy1t4FBssVk2V7EQEby_pRMriFaaBu6e"
+  CMS_TOKEN: process.env.NEXT_PUBLIC_CMS_TOKEN || "TFh5hvC6wApM0yGyIr7P9vtwnr_sbz1v"
 } as const;
